f9d32f6e8e79561abcbfa7bc06ebbd18c0205581
AhmedMaDaas <72462436+AhmedMaDaas@users.noreply.github.com>
2020-12-04 22:18:04 +0200

    Add files via upload

--- public/js/product.js
+++ public/js/product.js
@@ -1,74 +1,17 @@
 $(document).ready(function(){
+	$(window).resize(function(){
+	var l1=$(".slides .thumb-image").width();
+		$(".slides .thumb-image").height(l1);
+	});
+	$(".buynow").click(function(){
+		$(".buyynow").modal('show');
+		
+	});
+
 $(".products").each(function(){
 	if($(this).find(".row .product").length > 6)
 		$(this).find(".row .product:gt(5)").hide();
 });
-
-var okRateStar ="star-24px.svg";
-    var noRateStar ="star_border-24px.svg";
-    $(document).on('click', '.options .rating > img', function(){
-        var indexStar = $(this).index();
-        var src = $(this).attr('src');
-        let nameSplit = src.split("/");
-        let lastSplit = nameSplit[nameSplit.length - 1];
-        nameSplit.pop();
-        var newSrc = nameSplit.join('/');
-        //alert(newSrc);
-
-        if(lastSplit==okRateStar){
-            if(($(this).next("img").attr('src'))==newSrc.toString()+'/'+noRateStar || $(this).is(":last-of-type")){
-                $(this).parent().find(".index-star").val(indexStar-1);
-                $(this).attr('src',newSrc.toString()+'/'+noRateStar);
-            }else{
-                $(this).nextAll().filter("img").attr('src',newSrc.toString()+'/'+noRateStar);
-                $(this).parent().find(".index-star").val(indexStar);
-            }
-            
-        }else if(lastSplit==noRateStar){
-            $(this).attr('src',newSrc.toString()+'/'+okRateStar);
-            $(this).prevAll().filter("img").attr('src',newSrc.toString()+'/'+okRateStar);
-            $(this).nextAll().filter("img").attr('src',newSrc.toString()+'/'+noRateStar);
-            $(this).parent().find(".index-star").val(indexStar.toString());
-        }
-        var star = $(this).parent().find(".index-star").val();
-        console.log($(this).parent().find(".index-star").val());
-
-        var productId = $(this).parent().parent().parent().find(".product-id").val();
-        var meta = $(this).parent().parent().parent().find('meta[name="_token"]').attr('content');
-        var button = "evaluation";
-        alert(productId);
-
-        $.ajax({
-          url: "/home",
-          type: 'POST',
-          data: {
-            '_token' :meta,
-            'productId': productId,
-            'star' : star,
-            'button': button,
-          }, 
-          dataType: 'json',
-          success: function (response) {
-            alert(response.operation);
-            if(response.operation == 'login')location.href="/login";
-            // if(response.operation == 'success'){
-            //     thisVar.css("opacity","1");
-            //     thisVar.removeClass("filter-fairouzi");
-            //     thisVar.addClass("filter-orange");
-            // }else{
-            //     thisVar.css("opacity","0.5");
-            //     thisVar.removeClass("filter-orange");
-            //     thisVar.addClass("filter-fairouzi");
-            // }
-          },
-          error: function (response) {
-            alert("error ");
-            //location.href="{{route('log')}}";
-          },
-        });
-
-    });
-
     $("#products-sale .main-container .showall").click(function(){
         $(this).parent(".main-container").find(".products .row .product").slice(12).slideToggle(200);
         
@@ -105,6 +48,6 @@ var okRateStar ="star-24px.svg";
 		if($(this).siblings("button").hasClass("radio"))
 		$(this).siblings("button").removeClass("radio");
 	});
-    
+
 
 });
